3f684641ea59d4d7e4d29b5b1e8fb4c96881b500
Mohammed Maher Alhomsy <maheralhomsy2002@gmail.com>
2024-06-21 18:09:53 +0300

    add a dropdown menu for dalle screen

--- app/(auth)/(drawer)/dalle.tsx
+++ app/(auth)/(drawer)/dalle.tsx
@@ -1,9 +1,32 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+
+import { Stack } from "expo-router";
+
+import { defaultStyles } from "@/constants/Styles";
+import HeaderDropDown from "@/components/HeaderDropDown";
 
 const Page = () => {
   return (
-    <View>
-      <Text>Dalle Page</Text>
+    <View style={defaultStyles.pageContainer}>
+      <Stack.Screen
+        options={{
+          headerTitle: () => (
+            <HeaderDropDown
+              title="Dall.E"
+              onSelect={(key) => {}}
+              items={[
+                {
+                  key: "share",
+                  title: "Share GPT",
+                  icon: "square.and.arrow.up",
+                },
+                { key: "details", title: "See Details", icon: "info.circle" },
+                { key: "keep", title: "Keep in Sidebar", icon: "pin" },
+              ]}
+            />
+          ),
+        }}
+      />
     </View>
   );
 };
